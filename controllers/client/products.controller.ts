80520a19ae847c1bed4072319ea67cba85446db8
TrinhDinhHoang29 <162282399+TrinhDinhHoang29@users.noreply.github.com>
2024-07-17 10:21:25 +0700

    fix graphic in the web

--- controllers/client/products.controller.ts
+++ controllers/client/products.controller.ts
@@ -1,12 +1,22 @@
 import express,{Express, Request,Response} from 'express';
 import categorysModel from '../../models/product-categorys.model';
 import productsModel from '../../models/products.model';
+import reviewsModel from '../../models/reviews.model';
+import usersModel from '../../models/user.model';
 export const detail = async (req:Request,res:Response):Promise<void>=>{
     try{
         const slug = req.params.slug;
         const product = await productsModel.findOne({slug:slug,status:"active",deleted:false});
         const products = await productsModel.find({slug:{$ne:slug},productCategoryId:product.productCategoryId,status:"active",deleted:false}).limit(4);
-        res.render("client/pages/products/detail",{product,products});
+        const reviews:any = await reviewsModel.find({product_id:product._id}).select("-password -tokenUser ").lean();
+        const users = await usersModel.find({});
+        reviews.forEach(review=>{
+            const user = users.find(item=>item.id==review.user_id);
+            review.user = user;
+            const date = new Date(review.createdAt);
+            review.date = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+        })
+        res.render("client/pages/products/detail",{product,products,reviews:reviews});
     }catch(error){
         res.status(404).send("Page error"+error);
     }
